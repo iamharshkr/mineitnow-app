e6e55be98765461062fd1042697ce110ec85d332
Harsh Kumar <54257619+iamharshkr@users.noreply.github.com>
2023-10-01 19:19:14 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 var config = {};
 
-config.BASE_URL = 'https://mineitnow.herokuapp.com/api/v1/';
+config.BASE_URL = 'https://api.com/api/v1/';
 config.APP_NAME = 'MineItNow';
 config.ThemeColor = '#15AABF',
-config.InterstitialAdUnit = 'ca-app-pub-7294124022569088/9041945913';
-config.AppOpenAdUnit = 'ca-app-pub-7294124022569088/2044743985';
-config.BannerAdUnit = 'ca-app-pub-7294124022569088/2702115888';
-config.RewardedAdUnit = 'ca-app-pub-7294124022569088/7547124742';
+config.InterstitialAdUnit = 'ad-id';
+config.AppOpenAdUnit = 'ad-id';
+config.BannerAdUnit = 'ad-id';
+config.RewardedAdUnit = 'ad-id';
 
 
 module.exports = config;
